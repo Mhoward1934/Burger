a99fd19d1ecb1deff3f896c53a35af8eccdc5635
Monroe Howard II <monroehowardii@Monroes-MacBook-Air.local>
2019-01-09 22:54:32 -0600

    adding files and code for config controllers db models public and views

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken Swiss", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Meat Cheese", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Philly Cheese", false);
