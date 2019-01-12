1acdfdb0ba19fb9ccb8d8360546fa5d04cc3a6bb
Monroe Howard II <monroehowardii@Monroes-MacBook-Air.local>
2019-01-12 17:01:01 -0600

    added additional code

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var burger = require("../models/burger.js");
+var mySql = require("mysql");
 var router = express.Router();
 
 router.get("/", function(req, res) {
